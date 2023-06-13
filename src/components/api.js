f31c8398c7c1c770593eafeb2fec7e98156b027a
Aroua-zguira <arwa.zguira@gmail.com>
2023-06-13 20:33:04 +0100

    Initial commit

---
+++ src/components/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '24dcbed320msh46b0e501c7eb3c8p1e0b40jsn01526e6e86ee',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "f5a2041330a6c453a8d8682af2ca3078";
